d6c619eb776e9a9e2d68810f81c19a893ee74462
Sho Emoto <dd2d00seg1@gmail.com>
2021-11-11 20:44:20 +0900

    project追加

---
+++ Shared/swiftui_card_carousel_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_card_carousel_sampleApp.swift
+//  Shared
+//
+//  Created by Sho Emoto on 2021/11/11.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_card_carousel_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
